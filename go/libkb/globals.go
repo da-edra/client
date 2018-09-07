a37046a40a020eaa224eb67888ab57c88c8d703a
Joshua Blum <jblum18@gmail.com>
2018-09-07 13:11:28 -0400

    SetProofServices

--- go/libkb/globals.go
+++ go/libkb/globals.go
@@ -1026,7 +1026,7 @@ func (g *GlobalContext) MakeAssertionContext() AssertionContext {
 	return MakeAssertionContext(g.Services)
 }
 
-func (g *GlobalContext) SetServices(s ExternalServicesCollector) {
+func (g *GlobalContext) SetProofServices(s ExternalServicesCollector) {
 	g.Services = s
 }
 
